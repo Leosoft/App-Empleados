13d7034e3a085a8a05a232a29d279463ad244460
Leonardo Zabala <leosoft3031@gmail.com>
2021-06-28 11:39:15 -0300

    version 1.0

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Empleado } from './empleado.model';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titulo = 'Lista de Empleados';
+
+  empleados:Empleado[] = [
+
+    new Empleado("Leonardo","Zabala","Presidente",90000)
+  ]
+
+  agregarEmpleado(){
+
+    let miEmpleado=new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
+    this.empleados.push(miEmpleado);
+  }
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
+}
